e1c4231ef95a32f6997d1518b1ed1318635b9128
Camila <camila.verssao@gmail.com>
2025-06-19 13:45:39 -0300

    chore: ajustes de frontend ts

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-  
+    <p>cu</p>
   </div>
 </template>
 
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,25 +1,25 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+// import router from './router'
+// import store from './store'
 import axios from 'axios'
 
 axios.defaults.withCredentials = true
 
 const app = createApp(App)
 
-const requireComponent = require.context('./components', true, /\.vue$/)
+// const requireComponent = require.context('./components', true, /\.vue$/)
 
-requireComponent.keys().forEach((fileName) => {
-  const componentConfig = requireComponent(fileName)
-  const componentName = fileName
-    .split('/')
-    .pop()
-    .replace(/\.\w+$/, '')
-  app.component(componentName, componentConfig.default || componentConfig)
-})
+// requireComponent.keys().forEach((fileName) => {
+//   const componentConfig = requireComponent(fileName)
+//   const componentName = fileName
+//     .split('/')
+//     .pop()
+//     .replace(/\.\w+$/, '')
+//   app.component(componentName, componentConfig.default || componentConfig)
+// })
 
-app.use(store)
-app.use(router)
+// app.use(store)
+// app.use(router)
 
 app.mount('#app')
